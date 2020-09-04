c183742aa5855dd5db79b0dd48d4a635cad9b93f
Chayapol-c <chayapol.cha@ku.th>
2020-09-04 23:21:12 +0700

    django part 4

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,41 @@
-from django.http import HttpRequest , HttpResponse , Http404
+from django.http import HttpRequest , HttpResponse , Http404 ,HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404 , render
+from django.urls import reverse
+from django.views import generic
+from .models import Question ,Choice
 
-from .models import Question
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_quertset(self):
+        """return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.RedirectView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request , question_id):
+    question = get_object_or_404(Question , pk =question_id)
+    try:
+        selected_choice =question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError , Choice.DoesNotExist):
+        return render(request, 'polls/detail.html' , {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results' , args=(question_id,)))
 
 # Create your views here.
 def index(request):
@@ -25,8 +58,7 @@ def detail(request , question_id):
     return render(request , 'polls/detail.html' , {'question' : question})
 
 def result(request , question_id):
-    response = "You're looking at the results of question %s. "
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question , pk = question_id)
+    return render(request, 'poll/result.html' , {'question' , question})
 
-def vote(request , question_id):
-    return HttpResponse("You're voting on question %s."%question_id)
+
