f643c0b753277b953892cd89faf73e9f79fe5f9e
Chayapol-c <chayapol.cha@ku.th>
2020-09-18 18:38:09 +0700

    redirect to the polls index page for a poll where voting is not allowed,

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpRequest , HttpResponse , Http404 ,HttpResponseRedirect
+from django.http import HttpRequest , HttpResponse , Http404 ,HttpResponseRedirect 
 from django.template import loader
-from django.shortcuts import get_object_or_404 , render
+from django.shortcuts import get_object_or_404 , render , redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Question ,Choice
+from django.contrib import messages
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -20,7 +21,29 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def vote_for_poll(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if not question.can_vote():
+        messages.error(request, "This Question can not vote")
+        return redirect('polls:index')
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 # Generic View
@@ -49,20 +72,3 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
