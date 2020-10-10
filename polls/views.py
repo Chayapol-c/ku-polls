1b0db920e7f77ca72bdbe126beb26fa7f87abd15
Chayapol-c <chayapol.cha@ku.th>
2020-10-10 15:19:06 +0700

    edit views for coverage

--- polls/views.py
+++ polls/views.py
@@ -42,7 +42,7 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(questions.id,)))
 
 
 def vote_for_poll(request, question_id):
