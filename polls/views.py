bcdff6daaf014f9404560546eebc057a4f061c1f
Chayapol-c <chayapol.cha@ku.th>
2020-10-10 15:12:38 +0700

    edit views for coverage

--- polls/views.py
+++ polls/views.py
@@ -9,31 +9,31 @@ from django.contrib import messages
 def index(request):
     """Show list of questions."""
     latest_question_list = Question.objects.order_by('-pub_date')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    contexts = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', contexts)
 
 
 def detail(request, question_id):
     """Show detail of each selected question."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    questions = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': questions})
 
 
 def results(request, question_id):
     """Show result of selected question."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    questions = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': questions})
 
 
 def vote(request, question_id):
     """Update choice count when select."""
-    question = get_object_or_404(Question, pk=question_id)
+    questions = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = questions.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
-            'question': question,
+            'question': questions,
             'error_message': "You didn't select a choice.",
         })
     else:
@@ -42,14 +42,13 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results',
-                                            args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def vote_for_poll(request, question_id):
     """Show error message and redirect to index page if question is not allowed."""
-    question = get_object_or_404(Question, pk=question_id)
-    if not question.can_vote():
+    questions = get_object_or_404(Question, pk=question_id)
+    if not questions.can_vote():
         messages.error(request, "This Question can not vote")
         return redirect('polls:index')
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': questions})
