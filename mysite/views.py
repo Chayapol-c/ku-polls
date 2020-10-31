29e1a0ad6873ecd569d123f8d608149f9f122f3c
chayapol <chayapol.cha@ku.th>
2020-10-31 17:07:17 +0700

    remove unused view

--- mysite/views.py
+++ mysite/views.py
@@ -1,27 +1,8 @@
 """Views for mysite project."""
 
-from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import redirect
 
 
 def index(reqest):
     """Redirect to index page."""
     return redirect("polls:index")
-
-
-def signup(request):
-    """Register a new user."""
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_passwd = form.cleaned_data.get('password')
-            user = AuthenticationForm(username=username, password=raw_passwd)
-            # login(request, user)
-            return redirect('polls')
-        # what if form is not valid?
-        # we should display a message in signup.html
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
